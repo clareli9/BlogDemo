ffa641a8e28a1591a3b2eec4ba655042646c8468
clareli9 <li1033889482@gmail.com>
2020-02-01 22:39:32 -0500

    Update: model design

---
+++ src/main/java/com/clareli/demo/po/Tag.java
@@ -0,0 +1,34 @@
+package com.clareli.demo.po;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "t_tag")
+public class Tag {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    public Tag() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
