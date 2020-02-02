43636b360820dc72bdf6357c46a831addcca695d
clareli9 <li1033889482@gmail.com>
2020-02-01 21:05:24 -0500

    Update: Log process

--- src/main/java/com/clareli/demo/web/IndexController.java
+++ src/main/java/com/clareli/demo/web/IndexController.java
@@ -3,18 +3,20 @@ package com.clareli.demo.web;
 import com.clareli.demo.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name) {
+        /*
         String blog = null;
 
         if (blog == null) {
             throw new NotFoundException("Blog does not exsit");
-        }
+        } */
+        System.out.println("----index----");
         return "index";
     }
 }
